ff791ceed221dac1645ab7d233e67d2449d0df8e
bian17888 <bian17888@gmail.com>
2014-07-01 15:44:31 +0800

    add structure

---
+++ html/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>搜狐公共模块</title>
+<!--[if lt IE 9]>
+<script src="js/html5.js"></script>
+<![endif]-->
+<link rel="stylesheet" href="css/vane360.css" type="text/css" media="all" />
+</head>
+<body>
+</body>
+</html>
