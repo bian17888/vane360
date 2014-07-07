51013343cfb2640762c9a3f38a7a40c6cb349b7c
bian17888 <bian17888@gmail.com>
2014-07-07 16:46:48 +0800

    add all files V1.0

---
+++ html/041_ysyx_detail.php
@@ -0,0 +1,140 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>英国皇家学院</title>
+<!--[if lt IE 9]>
+<script src="js/html5.js"></script>
+<![endif]-->
+<link rel="stylesheet" href="css/vane360.css" type="text/css" media="all" />
+</head>
+<body>
+<!-- header（Start） -->
+<?php include 'header.html' ?>
+<!-- header（End） -->
+
+<div class="vane360-wrap ysyx-wrap">
+    <div class="bg-line" style="margin:25px auto 20px"></div>
+    <article class="clearfix"> 
+        <!-- main-cont-detail -->
+        <div class="ysyx-detail" >
+        	 <!-- ysyx-detail-top -->
+        	<section class="ysyx-detail-top">
+            	<h2 class="h2-big"><i class="m-icon m-icon-school"></i> <span>英国皇家学院</span></h2>
+            	<div class="m-title clearfix">
+                    <h2><i class="m-block-green"></i><span>概览</span></h2>
+                </div>
+                <div class="ysyx-detail-top-info clearfix">
+                	<div class="text">
+                    	<p class="school-logo"><img src="images/temp/pic_country_75x75_02.jpg" alt=""></p>
+                        <p class="school-title"><span chass="span-zh">英国皇家学院</span> <br /> <span class="span-en">The British Royal University</span></p>
+                        <p class="school-des">英国皇家学院是世界上唯一一所全部培养研究生的艺术与设计大学，并可以跨越这些学科授予文学硕士、哲学硕士和博士学位。伦敦帝国学院（Imperial College London，此乃最新的官方名称，原名Imperial College of Science, Technology and Medicine，又名帝国理工学院）成立於2008年，是隶属于伦敦大学的一个科学技术专精独立学院。帝国学院主校区称为南肯星顿校区，位于英国伦敦久负盛名的海德公园南边和皇家阿尔伯特大厅旁,伦敦的肯星顿和切尔西区交界处。</p>
+                    </div>
+                    <img class="school-pic" src="images/temp/pic_500x350_00.jpg" alt="">
+                </div>
+                <a class="btn-back" href="#"><i class="m-icon m-icon-back"></i><span>返回</span></a>
+            </section>
+        	 <!-- ysyx-detail-cont -->
+            <section class="ysyx-detail-cont">
+            	<ul>
+            		<li>
+                    	<div class="m-title clearfix">
+                            <h2><i class="m-block-green"></i><span>录取要求</span></h2>
+                        </div>
+                        <div class="info">
+                        	<p>① 申请人大学本科毕业，平均成绩80%，雅思总分不得低于7.0分。</p>
+                        	<p>② 申请材料包括简历、2封推荐信、成绩单以及PS(不超过两页)。</p>
+                        </div>
+                    </li>
+            		<li>
+                    	<div class="m-title clearfix">
+                            <h2><i class="m-block-green"></i><span>本科生专业</span></h2>
+                        </div>
+                        <div class="info">
+                        	<p>动画专业(BA)、音响专业(BA)、建筑设计专业、产品设计工程、视觉交流、造型艺术、传媒设计、室内设计。</p>
+                        </div>
+                    </li>
+                    <li>
+                    	<div class="m-title clearfix">
+                            <h2><i class="m-block-green"></i><span>研究生专业</span></h2>
+                        </div>
+                        <div class="info">
+                        	<p>工业设计转行业、建筑设计专业、产品设计工程、视觉交流、造型艺术、传媒设计。</p>
+                        </div>
+                    </li>
+                    <li>
+                    	<div class="m-title clearfix">
+                            <h2><i class="m-block-green"></i><span>风标定制课程</span></h2>
+                        </div>
+                        <div class="info">
+                        	<p><a href="#">点击查看针对英国皇家学院的定制课程设置 &gt;&gt;</a></p>
+                        </div>
+                    </li>
+                    <li>
+                    	<div class="m-title clearfix">
+                            <h2><i class="m-block-green"></i><span>成功案例</span></h2>
+                        </div>
+                        <div class="info">
+                            <p><a href="#">点击查看针对英国皇家学院的成功案例 &gt;&gt;</a></p>
+                        </div>
+                    </li>
+                    <li>
+                    	<div class="m-title clearfix">
+                            <h2><i class="m-block-green"></i><span>xxxxxx</span></h2>
+                        </div>
+                        <div class="info">
+                        	<p>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx。</p>
+                        </div>
+                    </li>
+                    <li>
+                    	<div class="m-title clearfix">
+                            <h2><i class="m-block-green"></i><span>xxxxxx</span></h2>
+                        </div>
+                        <div class="info">
+                        	<p>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx。</p>
+                        </div>
+                    </li>
+                    <li>
+                    	<div class="m-title clearfix">
+                            <h2><i class="m-block-green"></i><span>xxxxxx</span></h2>
+                        </div>
+                        <div class="info">
+                        	<p>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx。</p>
+                        </div>
+                    </li>
+                    <li>
+                    	<div class="m-title clearfix">
+                            <h2><i class="m-block-green"></i><span>xxxxxx</span></h2>
+                        </div>
+                        <div class="info">
+                        	<p>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx。</p>
+                        </div>
+                    </li>
+                    <li>
+                    	<div class="m-title clearfix">
+                            <h2><i class="m-block-green"></i><span>xxxxxx</span></h2>
+                        </div>
+                        <div class="info">
+                        	<p>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx。</p>
+                        </div>
+                    </li>
+            	</ul>
+            </section>
+        </div>
+    </article>
+    
+    <!-- main-cont -->
+    <section class="tool-bar">
+        <ul>
+            <li><a href="#"><i class="m-icon m-icon-zxbm"></i><span>在线报名</span></a></li>
+            <li><a href="#"><i class="m-icon m-icon-zxzx"></i><span>在线咨询</span></a></li>
+            <li><a href="#"><img src="images/bg_QRcode.png" alt=""></a></li>
+        </ul>
+    </section>
+</div>
+
+<!-- footer（Start） -->
+<?php include 'footer.html' ?>
+<!-- footer（End） -->
+</body>
+</html>
